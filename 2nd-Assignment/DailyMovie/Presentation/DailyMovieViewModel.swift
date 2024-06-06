d86e8412cde8b77497e618688cde10fdbf4ac5cd
cirtuare <cirtuare@gmail.com>
2024-06-07 04:48:17 +0900

    feat: DailyMovieViewModel 구현

---
+++ 2nd-Assignment/DailyMovie/Presentation/DailyMovieViewModel.swift
@@ -0,0 +1,47 @@
+//
+//  DailyMovieViewModel.swift
+//  2nd-Assignment
+//
+//  Created by 이수민 on 6/7/24.
+//
+
+import UIKit
+
+class DailyMovieViewModel {
+    private let useCase : DailyMovieUseCase
+    private let key = "95dd7589a2ca8b8126c77eabf642771a"
+    private let targetDt = "20240510" // Date.movieDate()
+    var dailyMovieInfo : [DailyMovieInfo] = []
+    
+    init(useCase: DailyMovieUseCase) {
+        self.useCase = useCase
+    }
+    
+    func setData(key: String, targetDt: String) {
+        useCase.getDailyMovie(key: key, targetDt: targetDt) { [weak self] response in
+            switch response{
+            case .success(let data):
+                let dailyMovieData = data.boxOfficeResult.dailyBoxOfficeList.map {
+                    DailyMovieInfo(rank: $0.rank, name: $0.movieNm, openDate: $0.openDt, audience: $0.audiAcc)
+                }
+                self?.handleDailyMovieData(data: dailyMovieData)
+                print("@Log - \(dailyMovieData)")
+            case .decodedErr:
+                print("decoding error")
+            case .networkFail:
+                print("network fail")
+            case .pathErr:
+                print("path error")
+            case .requestErr:
+                print("request error")
+            case .serverErr:
+                print("server error")
+            }
+        }
+    }
+    
+    func handleDailyMovieData(data: [DailyMovieInfo]) {
+        self.dailyMovieInfo = data
+    }
+
+}
--- 2nd-Assignment/DailyMovie/UI/ViewController/DailyMovieViewController.swift
+++ 2nd-Assignment/DailyMovie/UI/ViewController/DailyMovieViewController.swift
@@ -11,21 +11,23 @@ import Moya
 
 class DailyMovieViewController: UIViewController, UICollectionViewDelegate {
 
-    private let rootView = DailyMovieView()
-    private var collectionView : UICollectionView
+    private let rootView: DailyMovieView
+    private let viewModel: DailyMovieViewModel
+    private var collectionView: UICollectionView
     
     private var dailyMovieData: [DailyMovieInfo] = []
    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setData()
         register()
         setCollectionView()
     }
     
-    init() {
-        self.collectionView = rootView.collectionView
-        super.init(nibName: nil, bundle: nil)
+    init(rootView: DailyMovieView, viewModel: DailyMovieViewModel) {
+          self.rootView = rootView
+          self.viewModel = viewModel
+          self.collectionView = rootView.collectionView
+          super.init(nibName: nil, bundle: nil)
     }
     
     required init?(coder: NSCoder) {
@@ -47,37 +49,6 @@ class DailyMovieViewController: UIViewController, UICollectionViewDelegate {
             DailyMovieCollectionViewCell.self,
             forCellWithReuseIdentifier: DailyMovieCollectionViewCell.identifier
         )
-    }
-    
-    func setData() {
-        let key = "95dd7589a2ca8b8126c77eabf642771a"
-        let targetDt = "20240510" // Date.movieDate()
-
-        DailyMovieService.shared.getDailyMovie(key: key, targetDt: targetDt) { [weak self] response in
-            switch response{
-            case .success(let data):
-                // print("@Log - \(data)")
-                if let movieData = data as? DailyMovieResponse {
-                    let dailyMovieData = movieData.boxOfficeResult.dailyBoxOfficeList.map {
-                        DailyMovieInfo(rank: $0.rank, name: $0.movieNm, openDate: $0.openDt, audience: $0.audiAcc)
-                    }
-                    self?.handleDailyMovieData(dailyMovieData)
-                    print("@Log - \(dailyMovieData)")
-               } else {
-                   print("Decoding error / Unexpected data format")
-               }
-            case .decodedErr:
-                print("decoding error")
-            case .networkFail:
-                print("network fail")
-            case .pathErr:
-                print("path error")
-            case .requestErr:
-                print("request error")
-            case .serverErr:
-                print("server error")
-            }
-        }
     }
     
     func handleDailyMovieData(_ dailyMovieData: [DailyMovieInfo]) {
